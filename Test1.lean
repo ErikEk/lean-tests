90d39de676ae6dbf8d3171abea0662f0768ffa0e
ErikEk <erik.ek@gmail.com>
2025-04-16 15:57:23 +0200

    sg

--- Test1.lean
+++ Test1.lean
@@ -9,4 +9,16 @@ import Test1.Basic
 def m: Nat := 1
 def b1: Bool := true
 
+/- com -/
 #eval hello
+
+#check b1
+#check Nat → Nat
+#check Nat →  Nat → Nat
+#check Nat × Nat
+#check Nat × Nat → Nat
+#check (0, 1)
+#check (Nat → Nat) → Nat
+#check Nat.succ 2
+#eval Nat.succ 2 + 1
+#eval Nat.add 5 2
