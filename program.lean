b6fa871441e4dbb9610bcab14b91574292e1c689
ErikEk <erik.ek@gmail.com>
2025-04-25 20:47:51 +0200

    s

--- program.lean
+++ program.lean
@@ -1,3 +1,13 @@
+
+def twice (action : IO Unit) : IO Unit := do
+  action
+  action
+def nTimes (action : IO Unit) : Nat → IO Unit
+  | 0 => pure ()
+  | n + 1 => do
+    action
+    nTimes action n
+
 def main : IO Unit := do
   let stdin ← IO.getStdin
   let stdout ← IO.getStdout
@@ -7,6 +17,8 @@ def main : IO Unit := do
   let name := input.dropRightWhile Char.isWhitespace
 
   stdout.putStrLn s!"Hello, {name}!"
-
+  twice (IO.println "shy")
 
 #eval "Hello...   ".dropRightWhile (fun c => not (c.isAlphanum))
+
+#eval nTimes (IO.println "hello") 3
