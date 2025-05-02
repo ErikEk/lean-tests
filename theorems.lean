ecf56703d3216bb64e5e3917e4eb97e668226953
ErikEk <erik.ek@gmail.com>
2025-05-02 10:14:07 +0200

    s

--- theorems.lean
+++ theorems.lean
@@ -7,3 +7,5 @@ theorem andImpliesOr : A ∧ B → A ∨ B :=
     fun andEvidence =>
         match andEvidence with
         | And.intro a b => Or.inl a
+
+theorem onePlusOneAndLessThan : 1 + 1 = 2 ∨ 3 < 5 := by decide
