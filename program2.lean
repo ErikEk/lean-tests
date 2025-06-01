b239c36229752fe903c761d66671be59af2b0e83
ErikEk <erik.ek@gmail.com>
2025-06-01 17:21:45 +0700

    s g

--- program2.lean
+++ program2.lean
@@ -1,10 +1,16 @@
 import Mathlib
-
-open Real
-
+/-import Std.Data.Nat.Basic-/
+/-open Real-/
+open Nat
 #check 2
 
 def myFavNum : ℕ := 7
 
 example (a b : ℕ) : a + b = b + a := Nat.add_comm a b
 example (a b : ℕ) : a * b = b * a := Nat.mul_comm a b
+
+theorem add_zero_2 (n : ℕ) : n + 0 = n :=
+  Nat.add_zero n
+
+#check add_zero_2
+example (x : ℕ) := add_zero_2 x
