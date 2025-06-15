f82b5c0070db5d304bed3118067918b9a33b5b42
ErikEk <erik.ek@gmail.com>
2025-06-15 08:07:15 +0700

    s

--- program2.lean
+++ program2.lean
@@ -7,6 +7,7 @@ def myFavNum : ℕ := 7
 
 example (a b : ℕ) : a + b = b + a := Nat.add_comm a b
 example (a b : ℕ) : a * b = b * a := Nat.mul_comm a b
+example (a b : ℕ) : a * b = b * a := Nat.mul_des a b
 
 theorem add_zero_2 (n : ℕ) : n + 0 = n :=
   Nat.add_zero n
