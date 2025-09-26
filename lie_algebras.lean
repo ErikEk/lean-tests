725af7f2fa52e70429a80f5d59366f4ad6b08d5b
ErikEk <erik.ek@gmail.com>
2025-09-26 19:44:35 +0200

    s

--- lie_algebras.lean
+++ lie_algebras.lean
@@ -4,7 +4,6 @@ import Mathlib.Tactic
 variable (L M : Type) [LieRing L] [LieRing M]
 example : ∀ (x y z : L), ⁅x + y, z⁆ = ⁅x, z⁆ + ⁅y, z⁆ := by
   apply add_lie
-
 example : ∀ (x : L), ⁅x, x⁆ = 0 := by
   apply lie_self
 example : ∀ (x y m : L), ⁅x, ⁅y, m⁆⁆ = ⁅⁅x, y⁆, m⁆ + ⁅y, ⁅x, m⁆⁆ := by
