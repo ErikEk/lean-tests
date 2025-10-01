c369d3fc7f1b2b1ca0ee6980aa94d0a3f25f1950
ErikEk <erik.ek@gmail.com>
2025-10-01 19:49:53 +0200

    s

--- group_theory.lean
+++ group_theory.lean
@@ -1,4 +1,8 @@
 import Mathlib.Data.Real.Basic
+import Mathlib.Algebra.Group.Basic
 
 example {G : Type} [Group G] (g : G) : 1 * g = g := by
   apply one_mul
+example {G : Type} [Group G] (a b c : G) :
+    (a * b) * c = a * (b * c) := by
+    apply mul_assoc
