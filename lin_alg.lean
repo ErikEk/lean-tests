47ab45aea3cb935e2569ed986a1c60d79c719e98
ErikEk <erik.ek@gmail.com>
2026-01-10 07:41:05 +0100

    s

--- lin_alg.lean
+++ lin_alg.lean
@@ -8,3 +8,13 @@ example (x y : ℕ) (h : y = x + 7) : 2 * y = 2 * (x+7) := by
 example (P : Prop) : P → P := by
   intro a
   exact a
+example (P Q : Prop) (p: P) (q : Q) : P∧Q := by
+  constructor
+  exact p
+  exact q
+example (P Q : Prop) : (P → Q) → ¬Q → ¬P := by
+  unfold Not
+  intro a b h3
+  apply b
+  apply a
+  exact h3
