b5703384f6b98c8c3113dc4d37ecc36c7ffcf595
ErikEk <erik.ek@gmail.com>
2026-01-12 20:32:45 +0100

    s

--- lin_alg.lean
+++ lin_alg.lean
@@ -38,3 +38,9 @@ theorem smul_zero_v (a : K) : a • (0 : V) = (0 : V) := by
   apply add_right_cancel (b := a•(0:V))
   rw[(smul_add a (0:V) (0:V)).symm]
   rw [zero_add, zero_add]
+theorem neg_one_smul_v (v : V) : (-1 : K) • v = -v := by
+  apply add_right_cancel (b:=v)
+  nth_rw 2 [(one_smul K v).symm]
+  rw [(add_smul (-1:K) (1:K) v).symm]
+  rw [neg_add_cancel, neg_add_cancel]
+  exact zero_smul K v
