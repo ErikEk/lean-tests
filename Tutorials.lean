f649ec69b6fbd9b7f590e15d15f773e84ab0a375
ErikEk <erik.ek@gmail.com>
2026-03-28 17:16:57 +0100

    s

--- Tutorials.lean
+++ Tutorials.lean
@@ -1,3 +1,5 @@
 import Tutorials.content.«01EqualityRewriting»
 import Tutorials.content.«02IffIfAnd»
 import Tutorials.content.«03ForallOr»
+import Tutorials.content.«04Exists»
+import Tutorials.content.«05SequenceLimits»
