7b09c4febb204f9ebc4392500525769f3905b178
ErikEk <erik.ek@gmail.com>
2026-04-12 12:19:36 +0200

    s

--- program2.lean
+++ program2.lean
@@ -1,4 +1,5 @@
 import Mathlib
+
 open Nat
 #check 2
 
@@ -12,3 +13,12 @@ theorem add_zero_2 (n : ℕ) : n + 0 = n :=
 
 #check add_zero_2
 example (x : ℕ) := add_zero_2 x
+
+#check Nat.succ_eq_add_one 2
+example :  Nat.add 2 2 = 4 := by
+  unfold Nat.add
+  unfold Nat.add
+  unfold Nat.add
+  have h : Nat.succ 2 = 3 := by
+    exact Nat.succ_eq_add_one 2
+  rw [h]
