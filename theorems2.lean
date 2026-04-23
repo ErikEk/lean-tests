f8e53d3d9bff6ce898ad77c2fd190e05de9a8045
ErikEk <erik.ek@gmail.com>
2026-04-23 08:10:22 +0200

    s

--- theorems2.lean
+++ theorems2.lean
@@ -5,3 +5,48 @@ set_option diagnostics true
 open Finset
 open BigOperators -- make ∑ and ∏ notation available
 #eval Finset.sum (Finset.range 5) (fun i => i)   -- prints 10
+
+structure Account where
+  balance : Nat
+
+def withdraw (acc : Account) (amount : Nat) : Account × Bool :=
+  if h : amount ≤ acc.balance then
+    (⟨acc.balance - amount⟩, true)
+  else
+    (acc, false)
+
+-- If it returns true, the balance is reduced correctly
+theorem withdraw_ok
+  (acc : Account) (amount : Nat) (newAcc : Account) :
+  withdraw acc amount = (newAcc, true) →
+  newAcc.balance = acc.balance - amount :=
+by
+  intro h
+  unfold withdraw at h
+  by_cases hle : amount ≤ acc.balance
+  · simp [hle] at h
+    cases h
+    rfl
+  · simp [hle] at h
+
+-- If it returns false, balance is unchanged
+theorem withdraw_fail
+  (acc : Account) (amount : Nat) :
+  (withdraw acc amount).snd = false →
+  (withdraw acc amount).fst = acc :=
+by
+  intro h
+  unfold withdraw
+  by_cases hle : amount ≤ acc.balance
+  · simp [withdraw, hle] at h
+  · simp [hle]
+
+-- Balance never increases
+theorem withdraw_never_increases
+  (acc : Account) (amount : Nat) :
+  (withdraw acc amount).fst.balance ≤ acc.balance :=
+by
+  unfold withdraw
+  by_cases hle : amount ≤ acc.balance
+  · simp [hle]
+  · simp [hle]
