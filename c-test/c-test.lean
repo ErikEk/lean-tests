2bd0b8f330892edda98eb6040caac2fee531e05d
ErikEk <erik.ek@gmail.com>
2026-07-29 09:38:58 +0700

    s

--- c-test/c-test.lean
+++ c-test/c-test.lean
@@ -11,9 +11,22 @@ theorem abs_correct (x : Int) :
   absImpl x = absSpec x := by
   unfold absImpl absSpec
   rfl
-def add_32 (a b : Int32) : Int32 := a + b
 
-#eval add_32 5 4
+structure CInt32 where
+  val : Fin (2^32)
+deriving Repr
+def CInt32.add (x y : CInt32) : CInt32 :=
+  ⟨x.val + y.val⟩
+instance : HAdd CInt32 CInt32 CInt32 where
+  hAdd a b := ⟨a.val + b.val⟩
+def add_32 (a b : CInt32) : CInt32 := a + b
+
+def x : CInt32 := ⟨5⟩
+#eval add_32 x x
+
+/-theorem add_mod {a b : Int32} :
+  add_32 a b =
+    ((a.val + b.val) mod 2^32)-/
 
 def square (x : Int) : Int :=
   x * x
